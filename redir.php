0fbecd0d0924ff019d841dfe06c9a11b1ea4a91e
root <root@mail.dg-electronics.eu>
2015-09-24 00:55:15 +0200

    update

--- redir.php
+++ redir.php
@@ -1,28 +1,24 @@
 <?php
 
-
 $a = $_SERVER['REQUEST_METHOD'];
-/*
-//if(strtolower($a) == 'head')
-//{
-header("Content-Type: video/mp2t");
-header("Content-Disposition: attachment; filename=\"video.ts\"");
-//}
+
 
 $fp = fopen('bb.txt','a');
 fwrite($fp, $a."\n");
 fclose($fp);
 
-exit;
-*/
 if($a == 'HEAD')
 {
-//	exit;
+	header("Content-Type: video/mp2t");
+	header("Connection: close");
+	exit;
 }
-//if(strtolower($a) != 'head')
-//{
-	header("Location: http://s1.wssiptv.com:8000/live/WSSiptv.com/illilililIIIIilivwvwwwwwvw/92.ts?1234|User-agent=MXPlayer/1.7.38");
-//}
+
+
+if(strtolower($a) != 'head')
+{
+	header("Location: http://s1.wssiptv.com:8000/live/WSSiptv.com/illilililIIIIilivwvwwwwwvw/92.ts?1234");
+}
 
 
 ?>
